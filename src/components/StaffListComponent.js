d3bda4d922a89723baf3fe36e602fb7e129d8eec
leduc169 <letanduc96@gmail.com>
2021-09-10 21:26:39 +0700

    showName commit

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -9,7 +9,20 @@ const StaffList = () => {
 
     return(
         <>
-            
+            <div className="container-xl">
+                <div className="row">
+                    {STAFFS.map(e => 
+                        <div key={e.id} className="col-sm-6">
+                            <div className="cell" onClick={() => Info(e)}>{e.name}</div>
+                        </div>)}
+                </div>
+                <div className="row">
+                    
+                    <h5>Chọn vào nhân viên để xem thêm thông tin</h5>
+                </div>
+                
+            </div>
+            <hr></hr>
         </>
     )
 };
